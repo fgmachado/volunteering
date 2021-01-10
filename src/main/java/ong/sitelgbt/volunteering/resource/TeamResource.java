23f07c6cc0cc2f2f9c66be55eabb3e1a3ee66b6f
Felipe Gomes Machado <fgmachado0@gmail.com>
2021-01-10 13:56:49 -0300

    melhoria nos endpoints de time

--- src/main/java/ong/sitelgbt/volunteering/resource/TeamResource.java
+++ src/main/java/ong/sitelgbt/volunteering/resource/TeamResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,13 +24,20 @@ public class TeamResource {
     }
 
     @GetMapping("{id}")
-    public Optional<Team> findById(@PathVariable("id") Long id) {
-        return service.findById(id);
+    public ResponseEntity<Team> findById(@PathVariable("id") Long id) {
+        final Optional<Team> team = service.findById(id);
+
+        if (!team.isPresent()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(team.get());
     }
 
     @PostMapping
-    public Team add(@RequestBody Team team) {
-        return service.save(team);
+    public ResponseEntity<String> add(@RequestBody Team team) {
+        final Team entity = service.save(team);
+        return ResponseEntity.created(URI.create("/teams/" + entity.getId())).build();
     }
 
     @PutMapping
@@ -53,7 +61,7 @@ public class TeamResource {
         final Optional<Team> team = service.findById(id);
 
         if (!team.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            return ResponseEntity.noContent().build();
         }
 
         service.delete(team.get());
