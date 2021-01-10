b90b5b1abe91ec021383c42c54547a8339d31cf7
Felipe Gomes Machado <fgmachado0@gmail.com>
2021-01-10 01:23:20 -0300

    melhoria na documentação do endpoint de voluntários

--- src/main/java/ong/sitelgbt/volunteering/resource/VoluntaryResource.java
+++ src/main/java/ong/sitelgbt/volunteering/resource/VoluntaryResource.java
@@ -1,5 +1,8 @@
 package ong.sitelgbt.volunteering.resource;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import ong.sitelgbt.volunteering.model.Voluntary;
 import ong.sitelgbt.volunteering.service.VoluntaryService;
@@ -7,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,43 +21,65 @@ public class VoluntaryResource {
 
     private final VoluntaryService service;
 
+    @Operation(description = "List all registered volunteers")
+    @ApiResponse(responseCode = "200", description = "Returns a list of volunteers")
     @GetMapping
     public List<Voluntary> findAll() {
         return service.findAll();
     }
 
+    @Operation(description = "Get a voluntaty by id")
+    @ApiResponse(responseCode = "200", description = "A voluntary")
+    @ApiResponse(responseCode = "204", description = "If not found a voluntary")
     @GetMapping("{id}")
-    public Optional<Voluntary> findById(@PathVariable("id") Long id) {
-        return service.findById(id);
+    public ResponseEntity<Voluntary> findById(@PathVariable("id") Long id) {
+        final Optional<Voluntary> voluntary = service.findById(id);
+
+        if (!voluntary.isPresent()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(voluntary.get());
     }
 
+    @Operation(description = "Add a voluntaty")
+    @ApiResponse(responseCode = "201", description = "Returns if a voluntary is created")
     @PostMapping
-    public Voluntary add(@RequestBody Voluntary voluntary) {
-        return service.save(voluntary);
+    public ResponseEntity<String> add(@RequestBody Voluntary voluntary) {
+        final Voluntary entity = service.save(voluntary);
+        return ResponseEntity.created(URI.create("/volunteers/" + entity.getId())).build();
     }
 
+    @Operation(description = "Update a voluntary")
+    @ApiResponse(responseCode = "200", description = "Returns if a voluntary is updated")
     @PutMapping
-    public Voluntary update(@RequestBody Voluntary voluntary) {
-        return service.save(voluntary);
+    public ResponseEntity<Voluntary> update(@RequestBody Voluntary voluntary) {
+        return ResponseEntity.ok(service.save(voluntary));
     }
 
+    @Operation(description = "Deactivate a voluntary")
+    @ApiResponse(responseCode = "200", description = "If a voluntary is deactivated")
+    @ApiResponse(responseCode = "204", description = "If a voluntary is not found")
     @PatchMapping("{id}/disable")
     public ResponseEntity<Voluntary> deactivate(@PathVariable("id") Long id) {
         final Optional<Voluntary> register = service.findById(id);
 
         if (!register.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            return ResponseEntity.noContent().build();
         }
 
         return ResponseEntity.ok(service.deactivate(register.get()));
     }
 
+    @Operation(description = "Delete a voluntary")
+    @ApiResponse(responseCode = "200", description = "If a voluntary is deleted")
+    @ApiResponse(responseCode = "204", description = "If a voluntary is not found")
     @DeleteMapping("{id}")
     public ResponseEntity delete(@PathVariable("id") Long id) {
         final Optional<Voluntary> register = service.findById(id);
 
         if (!register.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            return ResponseEntity.noContent().build();
         }
 
         service.delete(register.get());
